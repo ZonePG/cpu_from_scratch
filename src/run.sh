712ff6c0aee66c9c7ba7895e2e77d01228fd19ed
ZonePG <619586533@qq.com>
2021-06-09 01:51:10 +0800

    chapter7 start from MacOS

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,3 @@
-% Start compiling %
-% iverilog -o  %
 iverilog -o wave def.v pc_reg.v if_id.v id.v regfile.v id_ex.v ex.v ex_mem.v mem.v mem_wb.v hilo_reg.v openmips.v inst_rom.v openmips_min_sopc.v openmips_min_sopc_tb.v
-% Generating wave file %
-vvp -n wave -lxt2 %
-% Opening wave file %
+vvp -n wave -lxt2
 gtkwave wave.vcd %
